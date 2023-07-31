a8ab66a355ae0b041a443f7699172e8942e4f405
Jerry Orta <jerryorta.dev@gmail.com>
2023-07-31 07:20:10 -0500

    add tailwind global config

--- apps/ngrx/todo/tailwind.config.js
+++ apps/ngrx/todo/tailwind.config.js
@@ -1,8 +1,12 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
+const { merge } = require('@ngpat/fn');
+
+// Add Global Config
+const sharedTailwindConfig = require('../../../libs/tailwind-preset/tailwind.config');
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = merge(sharedTailwindConfig, {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
@@ -11,4 +15,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+});
